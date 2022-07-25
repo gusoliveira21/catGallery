b47f1d271d295750b1f5e422577d22dbc0842eba
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2022-07-25 19:32:34 -0300

    Update code

--- app/src/main/java/br/com/gusoliveira21/catgallery/view/navigation/MainActivity.kt
+++ app/src/main/java/br/com/gusoliveira21/catgallery/view/navigation/MainActivity.kt
@@ -3,7 +3,6 @@ package br.com.gusoliveira21.catgallery.view.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.gusoliveira21.catgallery.R
-import org.koin.core.context.startKoin
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/test/java/br/com/gusoliveira21/catgallery/domain/usercase/GetCatImagesUseCaseTest.kt
+++ app/src/test/java/br/com/gusoliveira21/catgallery/domain/usercase/GetCatImagesUseCaseTest.kt
@@ -1,51 +1,42 @@
 package br.com.gusoliveira21.catgallery.domain.usercase
 
-import android.os.Bundle
-import android.util.Log
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import br.com.gusoliveira21.catgallery.MainCoroutineRule
 import br.com.gusoliveira21.catgallery.data.repository.CatRepository
-import br.com.gusoliveira21.catgallery.domain.entities.CatEntity
-import br.com.gusoliveira21.catgallery.extension.getOrAwaitValue
-import br.com.gusoliveira21.catgallery.view.ui.MainViewModel
-import br.com.gusoliveira21.catgallery.view.ui.MainViewModelUnitTest
 import br.com.gusoliveira21.catgallery.view.ui.catDataMocked
 import io.mockk.coEvery
-import io.mockk.coVerify
 import io.mockk.mockk
-import junit.framework.TestCase
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestWatcher
-import org.junit.runner.Description
 
-class GetCatImagesUseCaseTest : TestCase(){
-    @get:Rule
-    val rule = InstantTaskExecutorRule()
+/**
+ * Referencia: https://medium.com/swlh/unit-testing-with-kotlin-coroutines-the-android-way-19289838d257
+ *
+ * https://medium.com/google-developer-experts/unit-testing-kotlin-flow-76ea5f4282c5
+ */
+class GetCatImagesUseCaseTest {
+
+    @ExperimentalCoroutinesApi
     @get:Rule
     var coroutinesTestRule = MainCoroutineRule()
+
     private val repository = mockk<CatRepository>(relaxed = true)
 
-    private val testDispatcher = TestCoroutineDispatcher()
-    private val testScope = TestCoroutineScope(testDispatcher)
-
+    @ExperimentalCoroutinesApi
     @Test
-    fun `checks if the function return is a link`() {
-        testScope.launch {
-            // arrange
-            coEvery { repository.getData() } returns catDataMocked
-            val getCatImagesUseCaseTest = GetCatImagesUseCase(repository)
-            // act
-            val result = getCatImagesUseCaseTest.execute(Unit)
-            // assert
-            assertThat(result.success.data.first().image).isEqualTo("//i.imgur.com/8GkvlbT.jpgg")
-        }
+    fun `checks if the function return is a link`() = runBlocking {
+        // arrange
+        coEvery { repository.getData() } returns catDataMocked
+        val getCatImagesUseCaseTest = GetCatImagesUseCase(repository)
+
+        // act
+        val result = getCatImagesUseCaseTest.execute(Unit)
+
+        // assert
+        assertThat(result.success.data.first().image).isEqualTo("//i.imgur.com/8GkvlbT.jpg")
     }
 
 
