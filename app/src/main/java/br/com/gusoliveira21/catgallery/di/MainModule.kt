3b41baccd19c9afbcab056af5ef6776cfbe17e12
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2022-08-09 18:19:29 -0300

    deixa explícito quem CatService vai chamar
    Não era necessário, mas acho importante pra compreender melhor o funcionamento

--- app/src/main/java/br/com/gusoliveira21/catgallery/di/MainModule.kt
+++ app/src/main/java/br/com/gusoliveira21/catgallery/di/MainModule.kt
@@ -10,7 +10,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val mainModule = module {
-    single {
+    single<CatService> {
         RetrofitInicializer().getRetrofitService()
     }
 
