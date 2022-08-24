7da4593c1861eff36d1f73cf76629d8f22d615ca
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2022-08-24 20:35:54 -0300

    configura CatService para realizar uma busca por query

--- data/src/main/java/br/com/data/api/CatService.kt
+++ data/src/main/java/br/com/data/api/CatService.kt
@@ -1,18 +1,18 @@
 package br.com.data.api
 
-
-
 import br.com.data.modelResultRetrofit.CatDataClass
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface CatService {
     companion object{
-        const val BASE_URL = "3/gallery/search/?q=cats"
+        const val RESOURCE_URL_ORIGINAL = "3/gallery/search/?q=cats"
+        const val RESOURCE_URL = "3/gallery/search/"
         const val CLIENT_ID = "Authorization:  Client-ID 1ceddedc03a5d71"
     }
     @Headers(CLIENT_ID)
-    @GET(BASE_URL)
-    suspend fun catList(): CatDataClass
+    @GET(RESOURCE_URL)
+    suspend fun catList(@Query("q") params:String): CatDataClass
 
 }
