43db67c9d3ac439f97293f12f63e318a6101d78d
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2022-11-01 00:57:00 -0300

    atualiza FullscreenImageViewModel( )

--- app/src/main/java/br/com/gusoliveira21/catgallery/view/ui/fullscreenImageFragment/FullscreenImageViewModel.kt
+++ app/src/main/java/br/com/gusoliveira21/catgallery/view/ui/fullscreenImageFragment/FullscreenImageViewModel.kt
@@ -1,11 +1,11 @@
 package br.com.gusoliveira21.catgallery.view.ui.fullscreenImageFragment
 
 import android.content.Context
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.lifecycle.ViewModel
-import com.google.android.material.card.MaterialCardView
 
 abstract class FullscreenImageViewModel: ViewModel() {
     abstract fun setFullscreenImage(applicationContext: Context, imageSource: ImageView)
-    abstract fun setFullscreenImageWithPalletColor(context: Context, viewImagem: ImageView, cardView: MaterialCardView)
+    abstract fun setFullscreenImageWithPalletColor(context: Context, viewImagem: ImageView, cardView: FrameLayout)
 }
